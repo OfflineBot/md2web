ee0dc4ae011509583eee5b64d90568bad4fe86b4
OfflineBot <FlintStoneLG@gmail.com>
2024-03-10 12:13:34 +0100

    x

--- src/args/info.rs
+++ src/args/info.rs
@@ -1,7 +1,7 @@
 
 pub fn info() {
     println!("ob2web.exe");
-    println!("    |-> setup   | initialize settings.json");
+    println!("    |-> setup   | initialize folders");
     println!("    |-> create  | create the website");
     println!("\nRemember to name the main file index.md");
 }
